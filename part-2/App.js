ccb374506398eb39f7cf856840fc79454270e225
Christian Wise <djnarcissist@Christians-MBP.lan>
2023-10-26 12:05:27 -0400

    Part 2

---
+++ part-2/App.js
@@ -0,0 +1,18 @@
+function App () {
+    return (
+        <div>
+            <Tweet
+            name={"James Thomas"}
+            username={"JT_Money"}
+            date={new Date().toDateString()}
+            message={"OFWGFTKA"}
+            />
+            <Tweet
+            name={"Chris Carl"}
+            username={"KrissCross23"}
+            date={new Date().toDateString()}
+            message={"WELL HOW THE TURNTABLES!!!"}
+            />
+        </div>
+    );
+}
