0926302550966d66b4b74d482faa2e5d498d6177
Christian Wise <djnarcissist@Christians-MBP.lan>
2023-10-26 11:15:48 -0400

    Named Component

--- part-1/FirstComponent.js
+++ part-1/FirstComponent.js
@@ -2,7 +2,7 @@ import react from "react";
 
 
 const FirstComponent = () => {
-    return <h1>Very first first Component</h1>
+    return <h1>Very first first Component</h1>;
 };
 
 export default FirstComponent;
